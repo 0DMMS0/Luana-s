7157f1b3e0faa5b936bb90bd3c1ae3cb03788a61
mau <d.mau@outlook.es>
2026-09-15 09:13:26 -0600

    Serve PWA manifest and service worker

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+const CACHE='ikigai-shell-v1';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/','/manifest.webmanifest']))));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))});
